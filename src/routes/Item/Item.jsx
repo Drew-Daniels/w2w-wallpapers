9624a3edc3d2d85460439287498444a1119795a8
Drew Daniels <daniels_drew@protonmail.com>
2022-04-15 17:02:41 -0500

    Implemented loading icons.

--- src/routes/Item/Item.jsx
+++ src/routes/Item/Item.jsx
@@ -1,10 +1,64 @@
+// import components
+// react
+import { useState } from 'react';
+// react-icons components
+import {  IconContext } from 'react-icons';
+// import icons
+// react-icons icons
+import { 
+    BsFillCartDashFill as DecrCartIcon, 
+    BsFillCartPlusFill as IncrCartIcon 
+} from 'react-icons/bs';
+// react-bootstrap
+import { Spinner } from 'react-bootstrap';
+
+import { ButtonGroup, Button, Card } from 'react-bootstrap';
+
 import './Item.css';
 
 export function Item(props) {
 
+    const [loading, setLoading] = useState(true);
+    const { shopItem } = props;
+
     return (
-        <main>
-            Item
-        </main>
+        <li className='item'>
+            <Card>
+                <div className='image-container'>
+                    {loading &&
+                        <div className='loading-container'>
+                            <Spinner animation="grow" variant="primary" />
+                        </div>
+                    }
+                    <Card.Img 
+                        src={shopItem.imgURL} 
+                        style={ loading ? {display: 'none'} : {} }
+                        className='item-image'
+                        onLoad={() => {setLoading(false)}} 
+                    />
+                </div> 
+                <Card.Body>
+                    <Card.Title>{shopItem.brandName}</Card.Title>
+                    <div className='d-flex flex-column'>
+                        <a href={shopItem.brandURL}>{shopItem.brandURL}</a>
+                        <ButtonGroup>
+                            <Button>
+                                <IconContext.Provider value={{ size: '2em' }}>
+                                    <DecrCartIcon className='cart-icon'/>
+                                </IconContext.Provider>
+                                <span>Remove from Cart</span>
+                            </Button>
+                            <input value={shopItem.cartQty}></input>
+                            <Button>
+                                <IconContext.Provider value={{ size: '2em' }}>
+                                    <IncrCartIcon className='cart-icon'/>
+                                </IconContext.Provider>
+                                <span>Add to Cart</span>
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+                </Card.Body>
+            </Card>
+        </li>
     )
 }
--- src/routes/Items/Items.jsx
+++ src/routes/Items/Items.jsx
@@ -1,22 +1,10 @@
 // import components
-// react
-import { useState } from 'react';
 
 // react-router-dom
-import { Link, useOutletContext, useSearchParams, } from 'react-router-dom';
+import { useOutletContext, useSearchParams, } from 'react-router-dom';
 
-// react-bootstrap
-import { ButtonGroup, Button, Card } from 'react-bootstrap';
-
-// react-icons
-import {  IconContext } from 'react-icons';
-
-// import icons
-// react-icons
-import { 
-    BsFillCartDashFill as DecrCartIcon, 
-    BsFillCartPlusFill as IncrCartIcon 
-} from 'react-icons/bs';
+// my componets
+import { Item } from '../Item/Item';
 
 // import styling
 import './Items.css';
@@ -39,40 +27,9 @@ export function Items(props) {
                 {shopItems
                     .filter((shopItem) => matchesSearchParams(shopItem, 'category'))
                     .map((shopItem, i) => {
-                        return (
-                            <li key={i} className='item'>
-                                <Card>
-                                    <div className='image-container'>
-                                        <Card.Img src={shopItem.imgURL} className='item-image' loading='lazy' />
-                                    </div> 
-                                    <Card.Body>
-                                        <Card.Title>{shopItem.brandName}</Card.Title>
-                                        {/* <Card.Text>{shopItem.brandURL}</Card.Text> */}
-                                        <div className='d-flex flex-column'>
-                                            <a href={shopItem.brandURL}>{shopItem.brandURL}</a>
-                                            <ButtonGroup>
-                                                <Button>
-                                                    <IconContext.Provider value={{ size: '2em' }}>
-                                                        <DecrCartIcon className='cart-icon'/>
-                                                    </IconContext.Provider>
-                                                    <span>Remove from Cart</span>
-                                                </Button>
-                                                <input value={shopItem.cartQty}></input>
-                                                <Button>
-                                                    <IconContext.Provider value={{ size: '2em' }}>
-                                                        <IncrCartIcon className='cart-icon'/>
-                                                    </IconContext.Provider>
-                                                    <span>Add to Cart</span>
-                                                </Button>
-                                            </ButtonGroup>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </li>
-                        )
+                        return (<Item shopItem={shopItem} />)
                 })}
             </ul>
-            
         </main>
     )
 }
